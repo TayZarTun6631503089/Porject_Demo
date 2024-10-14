632814e0fbe898bd9112580804cc3c384699e5fd
TayZarTun6631503089 <156641441+TayZarTun6631503089@users.noreply.github.com>
2024-10-14 08:51:33 +0000

    kit_first

--- Pj-MVC/src/main/java/com/example/mvc/form/ScholarshipHistory.java
+++ Pj-MVC/src/main/java/com/example/mvc/form/ScholarshipHistory.java
@@ -2,4 +2,9 @@ package com.example.mvc.form;
 
 public class ScholarshipHistory {
 
+    private boolean scholarBefore;
+    private String ScholarName;
+    private int recieveYear;
+    private int amount;
+
 }
